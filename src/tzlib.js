480a152dfd0b507f067e1813d0fd768ceeff29e4
Jens Kuerschner <mail@jenskuerschner.de>
2022-08-31 21:08:54 +0200

    version update

--- src/tzlib.js
+++ src/tzlib.js
@@ -3,7 +3,7 @@
  * Add to Calendar TimeZones iCal Library
  * ++++++++++++++++++++++++++++++++++++++
  */
- const tzlibVersion = '1.1.4';
+ const tzlibVersion = '1.1.5';
 /* Creator: Jens Kuerschner (https://jenskuerschner.de)
  * Project: https://github.com/add2cal/timezones-ical-library
  * License: Apache-2.0
