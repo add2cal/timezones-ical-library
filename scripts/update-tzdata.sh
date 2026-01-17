11ef50d05fd03fc8002ac279a041cd41d417b9f1
Jens Kuerschner <code@jekuer.com>
2026-01-17 21:28:08 +0100

    fixing scripts

--- scripts/update-tzdata.sh
+++ scripts/update-tzdata.sh
@@ -3,6 +3,15 @@
 # check for the latest time zone data package at https://www.iana.org/time-zones
 # provide tzVersion (like 2025c) via argument: sudo sh update-tzdata.sh 2025c
 # provide "true" as second argument to also refresh vzic tool (not needed normally): sudo sh update-tzdata.sh 2025c true
+
+sudo_sed_inplace() {
+  if [ "$(uname)" = "Darwin" ]; then
+    sudo sed -i '' "$@"
+  else
+    sudo sed -i "$@"
+  fi
+}
+
 if [ "$1" ]
 then
   echo "👉 Updating ..."
@@ -31,16 +40,16 @@ then
     find . -type f ! -name 'Makefile' ! -name '*.c' ! -name '*.h' ! -name '*.pl' -delete
     # update file contents
     # in the Makefile, set TZID_PREFIX and PRODUCT_ID to empty
-    sudo sed -i '' 's/^TZID_PREFIX .*=.*$/TZID_PREFIX = /' Makefile
-    sudo sed -i '' 's/^PRODUCT_ID .*=.*$/PRODUCT_ID = /' Makefile
+    sudo_sed_inplace 's/^TZID_PREFIX .*=.*$/TZID_PREFIX = /' Makefile
+    sudo_sed_inplace 's/^PRODUCT_ID .*=.*$/PRODUCT_ID = /' Makefile
     # Fix unused argument warning by moving -L flag from CFLAGS to LDADD
-    sudo sed -i '' 's/ -L\/usr\/local\/lib64//' Makefile
-    sudo sed -i '' 's/^LIBICAL_LDADD = /LIBICAL_LDADD = -L\/usr\/local\/lib64 /' Makefile
+    sudo_sed_inplace 's/ -L\/usr\/local\/lib64//' Makefile
+    sudo_sed_inplace 's/^LIBICAL_LDADD = /LIBICAL_LDADD = -L\/usr\/local\/lib64 /' Makefile
     # in vzic-output.c, remove line "fprintf(fp, "BEGIN:VCALENDAR\r\nPRODID:%s\r\nVERSION:2.0\r\n", ProductID);" and "fprintf(fp, "END:VCALENDAR\r\n");"
-    sudo sed -i '' '/fprintf(fp, "BEGIN:VCALENDAR\\r\\nPRODID:%s\\r\\nVERSION:2.0\\r\\n", ProductID);/d' vzic-output.c
-    sudo sed -i '' '/fprintf(fp, "END:VCALENDAR\\r\\n");/d' vzic-output.c
+    sudo_sed_inplace '/fprintf(fp, "BEGIN:VCALENDAR\\r\\nPRODID:%s\\r\\nVERSION:2.0\\r\\n", ProductID);/d' vzic-output.c
+    sudo_sed_inplace '/fprintf(fp, "END:VCALENDAR\\r\\n");/d' vzic-output.c
     # in vzic-output.c, move "fprintf(fp, "X-LIC-LOCATION:%s\r\n", name);" before the line starting with "fprintf(fp, "LAST-MODIFIED:"
-    sudo sed -i '' -e '/fprintf(fp, "X-LIC-LOCATION:%s\\r\\n", name);/d' -e '/fprintf(fp, "LAST-MODIFIED:/i\
+    sudo_sed_inplace -e '/fprintf(fp, "X-LIC-LOCATION:%s\\r\\n", name);/d' -e '/fprintf(fp, "LAST-MODIFIED:/i\
     fprintf(fp, "X-LIC-LOCATION:%s\\r\\n", name);' vzic-output.c
     cd ..
     echo "✅ vzic tool updated"
